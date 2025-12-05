b3948cf16817425ed558b3311e13116592173285
Anna Karolynna <126431104+akarolynna@users.noreply.github.com>
2025-12-05 20:47:33 -0300

    Add program details and comments to Quantidade num positivos
    Added program description and usage instructions.

--- PASCALZINHO/WHILE/Quantidade num positivos.pas
+++ PASCALZINHO/WHILE/Quantidade num positivos.pas
@@ -1,22 +1,35 @@
+
+{
+🌟 DESAFIO DE PROGRAMAÇÃO - FUNDAMENTOS 💻
+------------------------------------------
+📅 Data de criação: 05/12/2025
+✍️ Autora: Anna Karolynna Mota 
+🏫 Instituto Federal Fluminense - Campus Itaperuna
+🎯 Linguagem: Pascalzinho
+
+✨ Digite vários números! O programa vai continuar funcionando enquanto você digitar números positivos ➕. Assim que você digitar um número zero ou negativo ⛔, o programa encerra e informa quantos números positivos foram digitados ao todo 🧮.
+
+}
 Program Pzim ;
 var
 num, qtdNum: integer;
 
 Begin
   qtdNum:=0;
-  write('Entre com um n�mero: ');
+  write('Entre com um número: ');
   readln(num);
   
   
   while (num >= 0) do
   begin
-	write('Entre com um n�mero: ');
+	write('Entre com um número: ');
   readln(num);
   
   qtdNum:= QtdNum +1;
   end;
   
-  writeln('QTD N� POSITIVOS:', qtdNum);
+  writeln('QTD N° POSITIVOS:', qtdNum);
   
   
+
 End.
