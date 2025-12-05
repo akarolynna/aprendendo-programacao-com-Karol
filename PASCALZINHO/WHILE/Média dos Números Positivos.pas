df072e78cc6d05af31c8c5941237a2a07767536a
Anna Karolynna <126431104+akarolynna@users.noreply.github.com>
2025-12-05 20:59:33 -0300

    Update Média dos Números Positivos.pas

--- PASCALZINHO/WHILE/Média dos Números Positivos.pas
+++ PASCALZINHO/WHILE/Média dos Números Positivos.pas
@@ -1,5 +1,21 @@
-Program Pzim ;
+{
+🌟 DESAFIO DE PROGRAMAÇÃO - FUNDAMENTOS 💻
+------------------------------------------
+📅 Data de criação: 05/12/2025
+✍️ Autora: Anna Karolynna Mota 
+🏫 Instituto Federal Fluminense - Campus Itaperuna
+🎯 Linguagem: Pascalzinho
 
+🧩ENUNCIADO:
+
+
+
+✨ Digite vários números positivos ➕. 
+O programa continuará aceitando números enquanto forem positivos.
+Quando você decidir parar (digitando um número zero ou negativo) ⛔, ele irá calcular e mostrar a média de todos os números positivos digitados 🧮.
+}
+
+Program Pzim ;
 var
 
 num, qtdNum, soma: integer;
@@ -9,16 +25,17 @@ Begin
   media:=0;
   soma:=0;
 	  
-	write('Entre com um n�mero: ');
+	write('Entre com um número: ');
   readln(num);
  
   while (num >=0) do
   begin
   qtdNum:= qtdNum +1;
   soma:=soma + num;
-	   write('Entre com um n�mero: ');
+	   write('Entre com um número: ');
      readln(num);
   end;
   media:= soma / qtdNum;
-  writeln('M�DIA DOS N�MEROS POSITIVOS: ', media:0:2);
+  writeln('MÉDIA DOS NÚMEROS POSITIVOS: ', media:0:2);
+
 End.
