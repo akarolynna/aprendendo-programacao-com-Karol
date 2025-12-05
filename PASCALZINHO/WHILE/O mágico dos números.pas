c7bd184d208f57b049d5ca622cc4f37c3ceb8767
Anna Karolynna <126431104+akarolynna@users.noreply.github.com>
2025-12-05 20:20:12 -0300

    Add program details and description for O mágico dos números
    Added program description and author information.

--- PASCALZINHO/WHILE/O mágico dos números.pas
+++ PASCALZINHO/WHILE/O mágico dos números.pas
@@ -1,3 +1,19 @@
+{
+🌟 DESAFIO DE PROGRAMAÇÃO - FUNDAMENTOS 💻
+------------------------------------------
+📅 Data de criação: 05/12/2025
+✍️ Autora: Anna Karolynna Mota 
+🏫 Instituto Federal Fluminense - Campus Itaperuna
+🎯 Linguagem: Pascalzinho
+
+DESCRIÇÃO:
+✨ O mágico da sala precisa da sua ajuda! 🧙‍♂️
+Ele pede que você digite vários números e, para cada número digitado, ele mostrará o triplo ✨ como se fosse um truque de mágica 🎩✨.
+Quando você quiser encerrar o show, digite -999 🚪.
+
+}
+
+
 Program Pzim ;
 var
 num, triplo: integer;
@@ -7,11 +23,12 @@ Begin
  
  while num <> (-999) do
  begin
-    write('Entre com um n�mero: ');
+    write('Entre com um número: ');
     readln(num);
     triplo:= num *3;
  
     writeln('TRIPLO -> ',num, ' X 3 = ', triplo);
     writeln();
  end; 
+
 End.
