12895b5377f7c1cadd91355e1691727854f8b303
Anna Karolynna <126431104+akarolynna@users.noreply.github.com>
2025-12-05 20:46:20 -0300

    Add files via upload

---
+++ PASCALZINHO/WHILE/Quantidade num positivos.pas
@@ -0,0 +1,22 @@
+Program Pzim ;
+var
+num, qtdNum: integer;
+
+Begin
+  qtdNum:=0;
+  write('Entre com um número: ');
+  readln(num);
+  
+  
+  while (num >= 0) do
+  begin
+	write('Entre com um número: ');
+  readln(num);
+  
+  qtdNum:= QtdNum +1;
+  end;
+  
+  writeln('QTD N° POSITIVOS:', qtdNum);
+  
+  
+End.
