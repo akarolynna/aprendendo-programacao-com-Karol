60094b3debe8b45c2ae36ca8f7bb8fa68c390fa4
Anna Karolynna <126431104+akarolynna@users.noreply.github.com>
2025-12-05 21:08:42 -0300

    Update Números entre 100 e 200.pas

--- PASCALZINHO/WHILE/Números entre 100 e 200.pas
+++ PASCALZINHO/WHILE/Números entre 100 e 200.pas
@@ -1,10 +1,23 @@
+{
+🌟 DESAFIO DE PROGRAMAÇÃO - FUNDAMENTOS 💻
+------------------------------------------
+📅 Data de criação: 05/12/2025
+✍️ Autora: Anna Karolynna Mota 
+🏫 Instituto Federal Fluminense - Campus Itaperuna
+🎯 Linguagem: Pascalzinho
+
+🧩ENUNCIADO:
+Leia vários números e informe quantos números entre 100 e 200 foram digitados. 
+Quando o valor 0 (zero) for lido, o algoritmo deverá cessar sua execução.
+
+}
 Program Pzim ;
 var
 num, qtd_num: integer;
 
 Begin
   qtd_num:=0;
-  write('Informe um n�mero: ');
+  write('Informe um número: ');
   readln(num);
   
   while (num <> 0) do
@@ -12,11 +25,12 @@ Begin
       if (num > 100) and (num < 200) then
       qtd_num:= qtd_num+1;
       
-      write('Informe um n�mero: ');
+      write('Informe um número: ');
       readln(num);
   end;
   
-  writeln('QTD DE N�MERO ENTRE 100 E 200: ', qtd_num);
+  writeln('QTD DE NÚMERO ENTRE 100 E 200: ', qtd_num);
   
   Readkey;
+
 End.
