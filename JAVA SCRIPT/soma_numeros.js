86ec8177786d68b804f6e05ea3daa9f8601e7fd2
Anna Karolynna <126431104+akarolynna@users.noreply.github.com>
2026-09-02 17:16:46 -0300

    Add program description and comments to soma_numeros.js
    Added program description and metadata comments.

--- JAVA SCRIPT/soma_numeros.js
+++ JAVA SCRIPT/soma_numeros.js
@@ -1,3 +1,18 @@
+/*
+
+💻 EXERCÍCIO DE PROGRAMAÇÃO — JAVASCRIPT
+==========================================
+📅 Data de criação: 02/09/2026
+👩‍💻 Autora: Anna Karolynna Mota
+🎓 Instituto Federal Fluminense — Campus Itaperuna
+📝 Linguagem: JavaScript
+
+🧩 DESCRIÇÃO
+Faça um programa que entre com vários números enquanto estes forem positivos, e ao final, imprima a soma de todos os números digitados.
+*/
+
+
+
 let soma = 0;
 let numeros = Number(prompt("Informe o número: "))
 while (numeros > 0){
