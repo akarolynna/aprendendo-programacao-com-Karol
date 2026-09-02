0079ccd78509f97e7dc89e9cd66ccb2573cbd7aa
Anna Karolynna <126431104+akarolynna@users.noreply.github.com>
2026-09-02 16:35:59 -0300

    Add files via upload

---
+++ JAVA SCRIPT/numeros_positivos.js
@@ -0,0 +1,16 @@
+let numero;
+/* Inicializamos a variável com 0, pois ela será usada como um contador. 
+Assim, garantimos que ela comece com 0, em vez de pegar algum valor
+aleatório que já estivesse na memória.*/
+let quantidade_numeros_pares = 0 
+
+numero = Number(prompt("Informe o número: "))   // O prompt() nos retorna uma String, por isso precisamos usar o Number (), pois ele permite que o número informado pelo usuário seja tratado como tipo númerico( englobando os números inteiros e reais)
+while (numero > 0){
+
+    quantidade_numeros_pares = quantidade_numeros_pares + 1 // Isso é a mesma coisa de fazermos:quantidade_numeros_pares += 1 ou quantidade_numeros_pares++
+    numero = Number(prompt("Informe o número: ")) 
+
+}
+
+console.log(`QTD Nº POSITIVOS: ${quantidade_numeros_pares}`)
+
