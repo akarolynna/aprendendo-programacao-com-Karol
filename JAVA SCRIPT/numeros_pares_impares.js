a570247c62d34c661f98febcb3f5b793d4d33b23
Anna Karolynna <126431104+akarolynna@users.noreply.github.com>
2026-09-07 16:58:43 -0300

    Restore and update comments in numeros_pares_impares.js

--- JAVA SCRIPT/numeros_pares_impares.js
+++ JAVA SCRIPT/numeros_pares_impares.js
@@ -7,8 +7,11 @@
 🎓 Instituto Federal Fluminense — Campus Itaperuna
 📝 Linguagem: JavaScript
 
-🧩 DESCRIÇÃO
+🧩 DESCRIÇÃO:
 
+Faça um programa que leia 10 números e informe quantos deles são pares e quantos são ímpares.
+
+--------------------------------------------------------------------------------
 Criamos as variáveis: qtd_numeros_pares e qtd_numeros_impares aqui em cima, pois assim elas podem ser
 acessadas fora do loop for, como foi no nosso caso. Se eu as criasse dentro de um bloco de comandos,
 aqueles que acompanham chaves {}, elas seriam apenas variáveis locais e só poderiam ser acessadas dentro
